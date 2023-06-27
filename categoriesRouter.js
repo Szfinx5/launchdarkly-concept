a8e2fc616ab236a4f0737778987c31cb78660e53
Gabor Havasi <g.havasi@outlook.com>
2023-06-27 17:05:47 +0100

    Removed the logs

--- categoriesRouter.js
+++ categoriesRouter.js
@@ -19,15 +19,11 @@ router.get("/:id", async function (req, res) {
   });
 
   if (launchDarklyResult.error) {
-    console.log("***error****");
-    console.log(launchDarklyResult);
     res.json({
       success: launchDarklyResult.success,
       error: launchDarklyResult.error,
     });
   } else {
-    console.log(id);
-    console.log(launchDarklyResult);
     const isCategoryCollapsed =
       launchDarklyResult.split(",").filter((e) => e === id).length > 0;
 
@@ -36,34 +32,6 @@ router.get("/:id", async function (req, res) {
       isCategoryCollapsed,
     });
   }
-
-  //
-  //     .then(function () {
-  //       ldClient.variation(
-  //         featureFlagKey,
-  //         context,
-  //         false,
-  //         function (err, flagValue) {
-  //           categoriesToCollapse = flagValue.split(",");
-  //           console.log(categoriesToCollapse);
-  //           res.json({
-  //             success: true,
-  //             payload: "Feature flag '" + featureFlagKey + "' is " + flagValue,
-  //           });
-
-  //           ldClient.flush(function () {
-  //             ldClient.close();
-  //           });
-  //         }
-  //       );
-  //     })
-  //     .catch(function (error) {
-  //       res.json({
-  //         success: false,
-  //         payload: "SDK failed to initialize: " + error,
-  //       });
-  //       process.exit(1);
-  //     });
 });
 
 export default router;
