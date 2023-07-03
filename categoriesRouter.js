9694851794cd6df2448f0ab36e83b43ddd3235c4
Gabor Havasi <g.havasi@outlook.com>
2023-07-03 16:05:03 +0100

    Fixed the import

--- categoriesRouter.js
+++ categoriesRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { launchDarklyQuery } from "./launchdarkly.js";
+import { launchDarklyQuery } from "./launchDarkly.js";
 
 const router = express.Router();
 router.use(express.json());
